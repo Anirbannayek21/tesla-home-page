667589b9777133180d7f3c0d45ddc7576cf15221
Anirbannayek21 <anirbannayek2@gmail.com>
2021-01-17 00:37:21 +0530

    init commit

---
+++ src/component/Header.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import './Header.css'
+import TeslaLogo from '../assets/teslaLogoSmall.svg'
+
+function header() {
+    return (
+        <div className='header'>
+            <div className="header-logo">
+                <img src={TeslaLogo} alt="tesla-logo"/>
+            </div>
+            <div className="header-center">
+                <p>Model S</p>
+                <p>Model 3</p>
+                <p>Model X</p>
+                <p>Model Y</p>
+                <p>Solar Roof</p>
+                <p>Solar Panels</p>
+            </div>
+            <div className="header-right">
+                <p>SHOP</p>
+                <p>TESLA ACOUNT</p>
+            </div>
+        </div>
+    )
+}
+
+export default header
